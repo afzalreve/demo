63adb0fafff93dc47dc209893a70adbaeee98e55
afzalsiddique <afzal.siddique99@gmail.com>
2024-12-31 14:50:56 +0600

    [not working] bind user dto from request (2)

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,12 +5,17 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping
     public UserDTO saveUser( @RequestBody UserDTO userDTO) {
@@ -23,4 +28,9 @@ public class UserController {
         return userService.updateUser(userDTO);
     }
 
+    @PostMapping("/form")
+    public UserDTO handleFormWithModelAttribute(@ModelAttribute UserDTO userDTO) {
+        logger.info(userDTO.toString());
+        return userDTO;
+    }
 }
